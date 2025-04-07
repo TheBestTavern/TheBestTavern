1893ec03143df118be3d6ee150ca79a5a17afb65
YJ402 <uky0916@gmail.com>
2025-04-07 15:14:07 +0900

    [feat] 시작 세팅: 폴더 트리, 모노 싱글톤

---
+++ Assets/01.Scripts/99.Utiles/MonoSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    [SerializeField] bool isDontDestroyOnLoad = false;
+
+    public static T Instace
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                string name = typeof(T).Name;
+                _instance = new GameObject(name).AddComponent<T>();
+                Debug.Log("싱글톤 생성");
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if(_instance != null && _instance != this)
+        {
+            Debug.Log("중복 생성된 싱글톤 객체 삭제");
+            Destroy(this);
+        }
+        else
+        {
+            _instance = this.gameObject.GetComponent<T>(); // 씬에 이미 배치된 경우 여기서 할당을 해줌.
+
+            if (isDontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(this);
+            }
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(_instance != null)
+        {
+            _instance = null;   
+        }
+    }
+}
