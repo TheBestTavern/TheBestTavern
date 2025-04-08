1b3fc26dbf76a419bd4fb900a9c059706e698e14
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-04-08 21:30:33 +0900

    [Feat((Wip)] 요리미니게임 (도마) 개발중
    미니게임 (도마) 기획 방향에 따라 수정예정

---
+++ Assets/01.Scripts/05.Cooking/CookingBoilMiniGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingBoilMiniGame :ICookingMiniGameHandler
+{
+    public void StartGame()
+    {
+    }
+
+    public void StopGame()
+    {
+    }
+
+    public void UpdateGame()
+    {
+    }
+
+    
+}
