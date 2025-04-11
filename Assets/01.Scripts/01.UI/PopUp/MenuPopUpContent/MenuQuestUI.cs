b3d7e817767ee1b57bf6df824431c0fb7f179c75
song7446 <songwon4529@naver.com>
2025-04-11 16:49:45 +0900

    [Feat] 메뉴 퀘스트 생성틀 추가

--- Assets/01.Scripts/01.UI/PopUp/MenuPopUpContent/MenuQuestUI.cs
+++ Assets/01.Scripts/01.UI/PopUp/MenuPopUpContent/MenuQuestUI.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class MenuQuestUI : MonoBehaviour
 {
+    [SerializeField] private QuestSlot questSlotPrefab;
+    [SerializeField] private Transform inProgressQuestParent;
+    [SerializeField] private Transform completedQuestParent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        SetInProgressQuests(); 
+        SetCompletedQuests();
+    }
+
+    void SetInProgressQuests()
+    {
+        for (int i = 0; i < QuestManager.Instance.questData.AcceptedQuests.Count; i++)
+        {
+            QuestSlot questSlot = Instantiate(questSlotPrefab, inProgressQuestParent).GetComponent<QuestSlot>();
+            questSlot.SetSlot(QuestManager.Instance.questData.AcceptedQuests[i], i);
+        }
+    }
+
+    void SetCompletedQuests()
+    {
+        for (int i = 0; i < QuestManager.Instance.questData.CompletedQuests.Count; i++)
+        {
+            QuestSlot questSlot = Instantiate(questSlotPrefab, completedQuestParent).GetComponent<QuestSlot>();
+            questSlot.SetSlot(QuestManager.Instance.questData.CompletedQuests[i], i);
+        }
     }
 }
