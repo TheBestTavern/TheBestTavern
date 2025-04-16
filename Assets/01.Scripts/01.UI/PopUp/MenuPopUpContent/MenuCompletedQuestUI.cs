03ec7f04c0b9700c50ea47c1750d0a5ea9aae3e6
song7446 <songwon4529@naver.com>
2025-04-16 09:40:51 +0900

    [Feat(Wip)] 픽스

--- Assets/01.Scripts/01.UI/PopUp/MenuPopUpContent/MenuCompletedQuestUI.cs
+++ Assets/01.Scripts/01.UI/PopUp/MenuPopUpContent/MenuCompletedQuestUI.cs
@@ -12,22 +12,22 @@ public class MenuCompletedQuestUI : BaseMenuContentUI
     /// </summary>
     public override void CreateContent()
     {
-        // 완료된 퀘스트가 없다면 리턴
-        if (QuestManager.Instance.questData.CompletedQuests == null)
-            return;
+        //// 완료된 퀘스트가 없다면 리턴
+        //if (QuestManager.Instance.questData.CompletedQuests == null)
+        //    return;
 
-        // 있다면 완료된 퀘스트 목록 순회
-        for (int i = 0; i < QuestManager.Instance.questData.CompletedQuests.Count; i++)
-        {
-            // To Do - 완료된 퀘스트 생성
-            if (QuestManager.Instance.questData.OnceCompletedQuests == null)
-                return;
+        //// 있다면 완료된 퀘스트 목록 순회
+        //for (int i = 0; i < QuestManager.Instance.questData.CompletedQuests.Count; i++)
+        //{
+        //    // To Do - 완료된 퀘스트 생성
+        //    if (QuestManager.Instance.questData.OnceCompletedQuests == null)
+        //        return;
 
-            for (int i = 0; i < QuestManager.Instance.questData.OnceCompletedQuests.Count; i++)
-            {
-                QuestOfferSlot questSlot = Instantiate(contentPrefab, contentParent).GetComponent<QuestOfferSlot>();
-                questSlot.SetSlot(QuestManager.Instance.questData.OnceCompletedQuests[i], i);
-            }
-        }
+        //    for (int i = 0; i < QuestManager.Instance.questData.OnceCompletedQuests.Count; i++)
+        //    {
+        //        QuestOfferSlot questSlot = Instantiate(contentPrefab, contentParent).GetComponent<QuestOfferSlot>();
+        //        questSlot.SetSlot(QuestManager.Instance.questData.OnceCompletedQuests[i], i);
+        //    }
+        //}
     }
 }
