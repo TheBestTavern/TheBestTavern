18670414af9202fed3384a1deb091b5e428c070b
song7446 <songwon4529@naver.com>
2025-04-18 19:21:11 +0900

    [Feat] 채집 미니게임 확률 추가 및 맷돌 미니게임 연결

--- Assets/01.Scripts/02.Gathering/GatheringProps.cs
+++ Assets/01.Scripts/02.Gathering/GatheringProps.cs
@@ -6,19 +6,46 @@ using UnityEngine;
 
 public class GatheringProps : MonoBehaviour
 {
+    bool isClicked = false;
+
     private void OnMouseEnter()
     {
-        gameObject.transform.DOScale(1.05f, 0.5f);
+        if (!isClicked)
+            gameObject.transform.DOScale(1.05f, 0.5f);
     }
 
     private void OnMouseExit()
     {
-        gameObject.transform.DOScale(1, 0.5f);
+        if (!isClicked)
+            gameObject.transform.DOScale(1, 0.5f);
     }
 
-    async private void OnMouseDown()
+    private void OnMouseDown()
     {
-        gameObject.transform.DOShakeScale(1f, 0.1f);
+        if (!isClicked)
+        {
+            gameObject.transform.DOShakeScale(1f, 0.1f);
+            int randint = Random.Range(0, 10);
+            if (randint == 0)
+            {
+                OnMiniGame();
+            }
+            else
+            {
+                GetItem();
+            }
+            gameObject.transform.DOScale(1, 0.5f);
+            isClicked = true;
+        }
+    }
+
+    public void GetItem()
+    {
+        Debug.Log("아이템 획득");
+    }
+
+    async void OnMiniGame()
+    {
         await SceneLoader.Instance.LoadSceneAsyncMiniGame("Forest_Animal");
     }
 }
