3fc7c25ade478e2546c5809247a9e8e66f5d272e
song7446 <songwon4529@naver.com>
2025-04-23 19:52:36 +0900

    [Fix] 맵 선택시 수정된 채집씬으로 이동및 불필요한 오브젝트 끄기

--- Assets/01.Scripts/01.UI/PopUp/SelectMapPopUp.cs
+++ Assets/01.Scripts/01.UI/PopUp/SelectMapPopUp.cs
@@ -81,7 +81,7 @@ public class SelectMapPopUp : BasePopUp
     async void ConfirmMoveSeaFunc()
     {       
         // 채집 씬으로 이동 
-        await SceneLoader.Instance.LoadSceneAsync("GatheringSceneDev");
+        await SceneLoader.Instance.LoadSceneAsync("GatheringSceneDev1");
     }
 
     // 확인 팝업 함수 
@@ -90,7 +90,7 @@ public class SelectMapPopUp : BasePopUp
         // 채집 씬으로 이동 
         SceneParameter.Set("Region", region);
         SceneParameter.Set("Season", DesignEnums.Season.winter);
-        await SceneLoader.Instance.LoadSceneAsync("GatheringSceneDev");
+        await SceneLoader.Instance.LoadSceneAsync("GatheringSceneDev1");
     }
 
     // 팝업 열때 필요한 함수
