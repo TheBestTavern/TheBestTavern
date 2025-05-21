716f7cdb0f5b5a63ba0291682fd0fd6150f18b08
RRoroan <tjdals8788@gmail.com>
2025-05-21 10:38:48 +0900

    [Feat] 어드레서블로 미끼 장전 스프라이트 불러오기

--- Assets/01.Scripts/02.Gathering/2D/BaitDropArea.cs
+++ Assets/01.Scripts/02.Gathering/2D/BaitDropArea.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,25 +18,54 @@ public class BaitDropArea : MonoBehaviour, IDropHandler, IPointerEnterHandler, I
     private InventorySlot previousSlot; 
     private ItemStack currentBait;
 
-    public void OnDrop(PointerEventData eventData)
+    public async void OnDrop(PointerEventData eventData)
     {
         InventorySlot draggedSlot = eventData.pointerDrag?.GetComponent<InventorySlot>();
+
         if (draggedSlot != null && draggedSlot.HasItem)
         {
             if (currentBait != null && previousSlot != null)
             {
                 previousSlot.GetSlotItem()?.Add(1, 10);
-                previousSlot.슬롯갱신(); 
+                previousSlot.슬롯갱신();
             }
 
             currentBait = draggedSlot.GetSlotItem();
             currentBait?.Subtract(1);
             draggedSlot.슬롯갱신();
-            previewImage.sprite = Resources.Load<Sprite>($"Item/{currentBait.Origin.englishName}");
-            previewImage.color = Color.white;
+
+            try
+            {
+                if (currentBait != null)
+                {
+                    Data_Foods raw = Data.GetRawItem(currentBait.Origin.key);
+                    string path = $"Assets/16.Image/FoodImage/{raw.englishName}.png";
+                    Sprite sprite = await AddressablesLoader.Instance.AddressablesLoadAsync<Sprite>(path, true);
+
+                    previewImage.sprite = sprite;
+                    previewImage.color = Color.white;
+                }
+                else
+                {
+                    ResetPreviewImage();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error loading bait image: {e}");
+                ResetPreviewImage();
+            }
+
             previousSlot = draggedSlot;
+
             throwController.SetBaitIndex(currentBait);
         }
+    }
+
+    private void ResetPreviewImage()
+    {
+        previewImage.sprite = null;
+        previewImage.color = new Color(1, 1, 1, 0);
     }
 
     public ItemStack GetCurrentBait() => currentBait;
