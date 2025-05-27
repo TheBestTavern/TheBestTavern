3e5d110b24f1ca60b265833ed2c87db17971473a
song7446 <songwon4529@naver.com>
2025-05-27 20:58:43 +0900

    [Fix] 빌드용 픽스

--- Assets/01.Scripts/08.Inventory/InventroySlot.cs
+++ Assets/01.Scripts/08.Inventory/InventroySlot.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class InventorySlot : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
