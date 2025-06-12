d4392d5358972e1068b17661ee2bca3ac078a504
song7446 <songwon4529@naver.com>
2025-06-12 16:44:34 +0900

    [Feat] 프레임 설정 추가

--- Assets/01.Scripts/01.UI/PopUp/ScreenResolutionPopUp.cs
+++ Assets/01.Scripts/01.UI/PopUp/ScreenResolutionPopUp.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class ScreenResolutionPopUp : BasePopUp
 {
+    [Header("화면 설정")]
     [SerializeField] private TMP_Dropdown resolutionDropdown;
     [SerializeField] private Toggle fullScreenToggle;
 
@@ -14,20 +17,41 @@ public class ScreenResolutionPopUp : BasePopUp
     private int currentResolutionIndex = 0;
     private bool isFullScreen;
 
+    [Header("품질 설정")]
     [SerializeField] private Slider qualitySlider;
     [SerializeField] private TextMeshProUGUI qualityText;
     private int currentQualityIndex = -1;
     private string[] qualityTexts = { "매우 낮음", "낮음", "보통", "높음", "매우 높음", "울트라" };
 
+    [Header("프레임 설정")]
+    [SerializeField] private Slider frameSlider;
+    [SerializeField] private TextMeshProUGUI frameText;
+    private int currentframeIndex = -1;
+    private int[] frames = { 30, 60, 90, 120, -1 };
+    [SerializeField] private Toggle vSyncCountToggle;
 
     private void Start()
+    {
+        InitScreenSet();
+        InitQualitySet();
+        InitFrameSet();
+
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+        fullScreenToggle.onValueChanged.AddListener(SetScreenMode);
+
+        qualitySlider.onValueChanged.AddListener(SetQuality);
+
+        frameSlider.onValueChanged.AddListener(SetFrame);
+        vSyncCountToggle.onValueChanged.AddListener(SetvSync);
+    }
+
+    private void InitScreenSet()
     {
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
 
         fullScreenToggle.isOn = Screen.fullScreen;
         isFullScreen = Screen.fullScreen;
-        fullScreenToggle.onValueChanged.AddListener(SetScreenMode);
 
         foreach (Resolution res in resolutions)
         {
@@ -55,33 +79,47 @@ public class ScreenResolutionPopUp : BasePopUp
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+    }
 
-        resolutionDropdown.onValueChanged.AddListener(SetResolution);
-
+    private void InitQualitySet()
+    {
         qualitySlider.wholeNumbers = true;
         qualitySlider.minValue = 0;
         qualitySlider.maxValue = QualitySettings.names.Length - 1;
-
-        qualitySlider.onValueChanged.AddListener(QualitySetting);
 
         currentQualityIndex = QualitySettings.GetQualityLevel();
         qualitySlider.value = currentQualityIndex;
         qualityText.text = qualityTexts[currentQualityIndex];
     }
 
-    public void SetResolution(int index)
+    private void InitFrameSet()
+    {
+        frameSlider.wholeNumbers = true;
+        frameSlider.minValue = 0;
+        frameSlider.maxValue = 4;
+
+
+        QualitySettings.vSyncCount = 0;
+        vSyncCountToggle.isOn = false;
+        currentframeIndex = 1;
+
+        SetFrame(currentframeIndex);
+        frameSlider.value = currentframeIndex;
+    }
+
+    private void SetResolution(int index)
     {
         Resolution res = res43List[index];
         Screen.SetResolution(res.width, res.height, isFullScreen);
     }
 
-    public void SetScreenMode(bool isFullScreen)
+    private void SetScreenMode(bool isFullScreen)
     {
         this.isFullScreen = isFullScreen;
         Screen.fullScreen = isFullScreen;
     }
 
-    public void QualitySetting(float value)
+    private void SetQuality(float value)
     {
         int index = Mathf.RoundToInt(value);
 
@@ -91,5 +129,20 @@ public class ScreenResolutionPopUp : BasePopUp
             QualitySettings.SetQualityLevel(index);
             qualityText.text = qualityTexts[index];
         }
+    }
+
+    private void SetFrame(float value)
+    {
+        currentframeIndex = Mathf.RoundToInt(value);
+        int frame = frames[currentframeIndex];
+        Application.targetFrameRate = frame;
+
+        frameText.text = frame == -1 ? "무제한" : $"{frame} FPS";
+    }
+
+    private void SetvSync(bool isvSync)
+    {
+        QualitySettings.vSyncCount = isvSync ? 1 : 0;
+        frameSlider.interactable = !isvSync;
     }
 }
